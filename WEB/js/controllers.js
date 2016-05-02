a4b0278ceb2c2ef8369f6e2f5549d84e4c794eac
sergio.tomio <sergio-2343@hotmail.com>
2016-05-02 18:25:21 -0300

    Coisas de Web

--- WEB/js/controllers.js
+++ WEB/js/controllers.js
@@ -1,5 +1,6 @@
 (function (angular) {
     'use strict';
+
     angular.module('busca', [])
         .controller('BuscaController', function () {
             this.campos = [
@@ -12,15 +13,39 @@
                 { nome: "Termos de Busca", id: "txTermosBusca" },
             ];
         });
-        
+
     angular.module('login', [])
-        .controller('LoginController', function() {
-           this.usuario = '';
-           this.senha = '';
-           
-            this.teste = function() {
-                var a = 1;
-                var b = 2;
+        .controller('LoginController', function () {
+            this.usuario;
+            this.senha;
+            this.confirmacaoSenha;
+            this.email;
+            this.modo = 'login';
+
+            this.mostrarDivCadastro = function () {
+                this.modo = 'cadastro';
+
+                this.trocarTextoHeader("Cadastro")
+                $("#btnConfirmarLogin").fadeOut(500);
+                $("#btnRegistrar").fadeOut(500, function () {
+                    $("#divCadastro").slideToggle(500);
+                });
+            }
+
+            this.esconderDivCadastro = function () {
+                this.modo = 'login';
+
+                this.trocarTextoHeader("Login");
+                $("#divCadastro").slideToggle(500, function () {
+                    $("#btnRegistrar").fadeIn(500);
+                    $("#btnConfirmarLogin").fadeIn(500);
+                });
+            }
+
+            this.trocarTextoHeader = function (novoTexto) {
+                $("#lblHeader").fadeOut(500, function () {
+                    $(this).text(novoTexto);
+                }).fadeIn();
             }
         });
 })(window.angular)

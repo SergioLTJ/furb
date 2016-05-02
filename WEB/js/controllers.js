fa8d231afb1dc921dab49a2a0cee4fbe04e83e31
Sérgio Tomio <Srgio Tomio>
2016-05-01 21:24:08 -0300

    Web - Ajustes.

--- WEB/js/controllers.js
+++ WEB/js/controllers.js
@@ -3,13 +3,24 @@
     angular.module('busca', [])
         .controller('BuscaController', function () {
             this.campos = [
-                { nome: "Título", id: "Titulo" },
-                { nome: "Banda", id: "Banda" },
-                { nome: "Artista", id: "Artista" },
-                { nome: "Gênero", id: "Genero" },
-                { nome: "Letra", id: "Letra" },
-                { nome: "Instrumento", id: "Instrumento" },
-                { nome: "Termos de Busca", id: "TermosBusca" },
+                { nome: "Título", id: "txTitulo" },
+                { nome: "Banda", id: "txBanda" },
+                { nome: "Artista", id: "txArtista" },
+                { nome: "Gênero", id: "txGenero" },
+                { nome: "Letra", id: "txLetra" },
+                { nome: "Instrumento", id: "txInstrumento" },
+                { nome: "Termos de Busca", id: "txTermosBusca" },
             ];
+        });
+        
+    angular.module('login', [])
+        .controller('LoginController', function() {
+           this.usuario = '';
+           this.senha = '';
+           
+            this.teste = function() {
+                var a = 1;
+                var b = 2;
+            }
         });
 })(window.angular)

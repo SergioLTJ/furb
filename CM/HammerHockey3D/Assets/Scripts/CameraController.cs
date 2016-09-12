94758c794d057fd9e9cee7bdbf610c176985e189
Guilherme Diegoli <gd_neto@hotmail.com>
2016-09-12 18:51:47 -0300

    Projeto HammerHockey3D (por enquanto é o roll-a-ball do tutorial)

---
+++ CM/HammerHockey3D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject playerObject;
+
+    private Vector3 offset;
+
+	void Start () {
+        offset = transform.position - playerObject.transform.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = playerObject.transform.position + offset;
+	}
+}

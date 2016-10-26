5260c488f0f487d89505a492d31b81734994230b
Guilherme Diegoli <gd_neto@hotmail.com>
2016-10-26 21:19:10 -0200

    Tabuleiro gerado + passos do jogador

---
+++ CM/HammerHockey3D/Assets/Classes/VerticeTabuleiro.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace Assets.Classes
+{
+    public enum TipoVertice
+    {
+        inicio,
+        intermediario,
+        final,
+    }
+
+    public enum DirecaoAvanco
+    {
+        cima,
+        baixo,
+        direita,
+        esquerda,
+        nenhum,
+    }
+
+    public class VerticeTabuleiro
+    {
+        public const float TRANSLACAO = 2.2f;
+
+        public VerticeTabuleiro proximo { get; private set; }
+        public DirecaoAvanco direcao { get; private set; }
+ 
+        public TipoVertice tipo { get; private set; }
+
+        public VerticeTabuleiro(TipoVertice tipo)
+        {
+            this.tipo = tipo;
+            direcao = DirecaoAvanco.nenhum;
+        }
+
+        public VerticeTabuleiro(TipoVertice tipo, VerticeTabuleiro pai, DirecaoAvanco direcaoPai)
+        {
+            this.tipo = tipo;
+
+            pai.direcao = direcaoPai;
+            pai.proximo = this;
+        }
+
+        public Vector3 GetTranslacao()
+        {
+            float x = 0;
+            float y = 0;
+            float z = 0;
+
+            switch (direcao)
+            {
+                case DirecaoAvanco.cima:
+                    z -= TRANSLACAO;
+                    break;
+                case DirecaoAvanco.baixo:
+                    z += TRANSLACAO;
+                    break;
+                case DirecaoAvanco.direita:
+                    x -= TRANSLACAO;
+                    break;
+                case DirecaoAvanco.esquerda:
+                    x += TRANSLACAO;
+                    break;
+            }
+
+            return new Vector3(x, y, z);
+        }
+    }
+}
---
+++ CM/HammerHockey3D/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using Assets.Classes;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    VerticeTabuleiro casaAtual;
+    public Text textoFinal;
+
+    // Use this for initialization
+    void Start()
+    {
+	}
+	
+	void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (casaAtual.tipo == TipoVertice.final)
+            {
+                textoFinal.text = "FIM!";
+            }
+            else 
+            {
+                transform.position += casaAtual.GetTranslacao();
+                casaAtual = casaAtual.proximo;
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+    }
+
+    public void SetCasaInicial(VerticeTabuleiro vertice)
+    {
+        casaAtual = vertice;
+    }
+}

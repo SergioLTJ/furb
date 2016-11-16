63f018472a92d8cfa39fe17b3d74e174f65cc296
Sérgio Tomio <Srgio Tomio>
2016-11-15 22:26:06 -0200

    Mada Mada

---
+++ TesteSocket/TesteSocket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace TesteSocket
+{
+    public class Comunicacao : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://0.0.0.0");
+            wssv.AddWebSocketService<Comunicacao>("/Comunicacao");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
+
